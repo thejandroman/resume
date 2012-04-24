31737566caac8a2503086ad5b5ffc8966c3d0789
Alejandro Figueroa <thejandroman@gmail.com>
2012-04-23 22:05:56 -0400

    This works now
    login.php should show some basics of how linked in api works from the
    command line.

--- login.php
+++ login.php
@@ -9,26 +9,41 @@ include("helpers.php");
 // create a new instance of the OAuth PECL extension class
 $oauth = new OAuth(API_CONSUMER_KEY, API_CONSUMER_SECRET);
 
-// get our request token
-$api_url = "https://api.linkedin.com/uas/oauth/requestToken";
-$rt_info = $oauth->getRequestToken($api_url);
+$oauth->enableDebug();
 
-// now set the token so we can get our access token
-$oauth->setToken($rt_info["oauth_token"], $rt_info["oauth_token_secret"]);
+if( file_exists(".service.dat") ) {
+  $config = json_decode(file_get_contents(".service.dat"));
+  if( isset($config->oauth_token) && isset($config->oauth_token_secret) ) {
+    $oauth->setToken($config->oauth_token, $config->oauth_token_secret);
+  } else {
+    print_line("We had a service.dat file, but it didn't contain a token/secret?", null, STDERR, __LINE__);
+  }
+} else {
+  print("We don't have a service.dat file, so we need to get access tokens!");
 
-// instruct on how to authorize the app
-print("Please visit this URL:\n\n");
-printf("https://www.linkedin.com/uas/oauth/authenticate?oauth_token=%s", $rt_info["oauth_token"]);
-print("\n\nIn your browser and then input the numerical code you are provided here: ");
+  $request_token_info = $oauth->getRequestToken("https://api.linkedin.com/uas/oauth/requestToken");
+  if( $request_token_info === FALSE || empty($request_token_info) ) {
+    print_line("Failed to fetch request token, debug info: %s", print_r($oauth->debugInfo, true), STDERR, __LINE__);
+  }
 
-// ask for the pin  
-$pin = trim(fgets(STDIN));
+  $oauth->setToken($request_token_info["oauth_token"], $request_token_info["oauth_token_secret"]);
 
-// get the access token now that we have the verifier pin
-$at_info = $oauth->getAccessToken("https://api.linkedin.com/uas/oauth/accessToken", "", $pin);
+  print_line("Please visit this URL:
+		\nhttps://www.linkedin.com/uas/oauth/authenticate?oauth_token=%s
+		\nIn your browser and then input the numerical code you are provided here: ", $request_token_info["oauth_token"]);
 
-// set the access token so we can make authenticated requests
-$oauth->setToken($at_info["oauth_token"], $at_info["oauth_token_secret"]);
+  $pin = trim(fgets(STDIN));
+
+  $access_token_info = $oauth->getAccessToken("https://api.linkedin.com/uas/oauth/accessToken", "", $pin);
+  if( $access_token_info === FALSE || empty($access_token_info) ) {
+    print("Failed to fetch access token, debug info:");
+    die(print_r($oauth->debugInfo, true));
+  }
+
+  $oauth->setToken($access_token_info["oauth_token"], $access_token_info["oauth_token_secret"]);
+
+  file_put_contents(".service.dat", json_encode($access_token_info));
+}
 
 // do a simple query to make sure our token works
 // we fetch our own profile on linkedin. This query will be explained more on later pages
@@ -41,10 +56,8 @@ print_response($oauth);
 
 print_line("\n********A basic user profile call********");
 $api_url = "http://api.linkedin.com/v1/people/~:(first-name,last-name,positions)";
-//echo "$api_url\n";
 $oauth->fetch($api_url, null, OAUTH_HTTP_METHOD_GET);
 // JSON
 // $oauth->fetch($api_url, null, OAUTH_HTTP_METHOD_GET, array('x-li-format' => 'json'));
 $response = $oauth->getLastResponse(); // just a sample of how you would get the response
-print_r($response);
 print_response($oauth);
