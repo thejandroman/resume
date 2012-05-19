5a583a7321dfb55b9ed5a8b8c93d0641efee04a9
mike anderson <singularityneuromancer@gmail.com>
2012-05-19 11:47:58 -0400

    moving to laptop

--- index.php
+++ index.php
@@ -7,161 +7,7 @@ if($result === false)
 ?>
 <html>
 <head>
-<script type="text/javascript" src="http://platform.linkedin.com/in.js">
-/*api_key: j18qrld132fh
-  authorize: true*/
-  api_key: q166216s6ikx
-  authorize: true
-  </script>
-  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script> 
-  <script type="text/javascript">
-      //$(document).ready(function(){
-  function loadData() {
-    IN.API.Profile("me")
-    .fields(["id", "publicProfileUrl", "firstName", "lastName",
-             "pictureUrl", "headline", "positions", "skills","location:(name)","phone-numbers","main-address","educations"])
-    .result(function(result) {
-var values = new Array();
-	function process(key,value,parent) {
-	if(typeof(value)!='object')
-	{
-	var final_key = (parent+'.'+key).replace(/[^a-zA-Z.]+/g,'').replace('.values','').replace('..','.');
-	//if(final_key.containis(.total))
-	if(values[final_key] === undefined)
-		values[final_key] = new Array();
-	values[final_key].push(value);
-	}
-	
-		
-		
-		
-	}
 
-function traverse(o,func, parent) {
-    for (i in o) {
-			func.apply(this,[i,o[i],parent]); 
-
-        if (typeof(o[i])=="object") {
-            traverse(o[i],func,parent + '.' + i);
-        }
-    }
-}
-
-//that's all... no magic, no bloated framework
-traverse(result.values[0],process,'');
-
-for(key in values)
-{
-	
-}
-for(key in values)
-{
-for(var i = 0; i < values[key].length; i++)
-	{
-		$('#resume').contents().filter(function() {
-  return this.html().contains(key);
-})
-	}
-}
-
-/*
-			$("#profile").html(JSON.stringify(result));
-			$('#template').html(<?php echo "'".$template."'";?>);
-			var skill = $('.skill').first();
-			var education = $('.education').first();
-			var position = $('.position').first();
-			skill.remove();
-			education.remove();
-			position.remove();
-			for(var i=0;i<result.values[0].skills._total;i++)
-			{
-				var this_skill = skill.clone();
-				if(result.values[0].skills.values[i].skill.name)
-					{
-					var element = this_skill.children('.name').first();
-					element.html(result.values[0].skills.values[i].skill.name + element.html());
-				$('.skills').append(this_skill);
-				}
-			}
-			for(var i=0;i<result.values[0].educations._total;i++)
-			{
-				var this_education = education.clone();
-				if(result.values[0].educations.values[i].schoolName)
-				{
-				var element = this_education.children('.schoolName').first();
-				element.html(result.values[0].educations.values[i].schoolName + element.html());
-				}
-				if(result.values[0].educations.values[i].degree)
-				{
-				var element = this_education.children('.degree').first();
-				element.html(result.values[0].educations.values[i].degree + element.html());
-				}
-				if(result.values[0].educations.values[i].fieldOfStudy)
-				{
-				var element = this_education.children('.fieldOfStudy').first();
-				element.html(result.values[0].educations.values[i].fieldOfStudy + element.html());
-				}
-				if(result.values[0].educations.values[i].endDate.year)
-				{
-var element = this_education.children('.endDate').first().children('.year').first();
-				element.html(result.values[0].educations.values[i].endDate.year + element.html());
-								}
-				$('.educations').append(this_education);
-			}
-			for(var i=0;i<result.values[0].positions._total;i++)
-			{
-				var this_position = position.clone();
-				if(result.values[0].positions.values[i].company.name)
-				{
-				var element = this_position.children('.company').first().children('.name').first();
-				element.html(result.values[0].positions.values[i].company.name + element.html());
-				}
-				if(result.values[0].positions.values[i].startDate)
-				{
-				if(result.values[0].positions.values[i].startDate.month)
-								{
-				var element = this_position.children('.startDate').first().children('.month').first();
-				element.html(result.values[0].positions.values[i].startDate.month + element.html());
-				}
-				if(result.values[0].positions.values[i].startDate.year)
-				{
-				var element = this_position.children('.startDate').first().children('.year').first();
-				element.html(result.values[0].positions.values[i].startDate.year + element.html());
-				}
-				}
-				if(result.values[0].positions.values[i].endDate)
-				{
-				if(result.values[0].positions.values[i].endDate.month)
-								{
-				var element = this_position.children('.endDate').first().children('.month').first();
-				element.html(result.values[0].positions.values[i].endDate.month + element.html());
-				}
-				if(result.values[0].positions.values[i].endDate.year)
-				{
-				var element = this_position.children('.endDate').first().children('.year').first();
-				element.html(result.values[0].positions.values[i].endDate.year + element.html());
-				}
-				}
-				if(result.values[0].positions.values[i].summary)
-								{
-				var element = this_position.children('.summary').first();
-				element.html(result.values[0].positions.values[i].summary + element.html());
-				}
-				if(result.values[0].positions.values[i].title)
-								{
-				var element = this_position.children('.title').first();
-				element.html(result.values[0].positions.values[i].title + element.html());
-				}
-				$('.positions').append(this_position);
-			}
-			$('.firstName').text(result.values[0].firstName);
-			$('.lastName').text(result.values[0].lastName);
-			$('.mainAddress').text(result.values[0].mainAddress);
-        });
-  */});
-}
-  </script>
 </head>
 <body>
 <script type="in/Login" data-onAuth="loadData"></script>
@@ -178,6 +24,110 @@ var element = this_education.children('.endDate').first().children('.year').firs
   #phone, #name, #email{margin:0px 5px;}
   </style>
   <div id="profile"></div>
-<div id="template"></div>
+<div id="template"><?php echo  "'".$template."'" ?></div>
+<script type="text/javascript" src="http://platform.linkedin.com/in.js">
+/*api_key: j18qrld132fh
+  authorize: true*/
+  api_key: q166216s6ikx
+  authorize: true
+  </script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script> 
+  <script type="text/javascript">
+  	function loadData()
+	{
+		IN.API.Profile("me").fields(["id", "publicProfileUrl", "firstName", "lastName", "pictureUrl", "headline", "positions", "skills", "location:(name)", "phone-numbers", "main-address", "educations"]).result(function (result)
+		{
+			var values = new Array();
+
+			function process(key, value, parent)
+			{
+				if (typeof (value) != 'object')
+				{
+					var final_key = (parent + '.' + key).replace(/[^a-zA-Z.]+/g, '').replace('.values', '').replace('..', '.').substr(1);
+					//if(final_key.containis(.total))
+					if (values[final_key] === undefined) values[final_key] = new Array();
+					values[final_key].push(value);
+				}
+			}
+
+			function traverse(o, func, parent)
+			{
+				for (i in o)
+				{
+					func.apply(this, [i, o[i], parent]);
+					if (typeof (o[i]) == "object")
+					{
+						traverse(o[i], func, parent + '.' + i);
+					}
+				}
+			}
+			//that's all... no magic, no bloated framework
+			traverse(result.values[0], process, '');
+			for (key in values)
+			{
+				for (var i = 0; i < values[key].length; i++)
+				{
+					//document.write(key + ':' + values[key][i] + '<br />')
+				}
+			}
+			 $(document).ready(function ()
+{
+
+			var templates = $('#resume').html().match(/\[template:[a-zA-Z\.]+(\((.)+?\))*(\])/g); 
+			for (var i = 0; i < templates.length; i++)
+			{
+				document.write(templates[i] + '<br />');
+				var parameters = templates[i].match(/\((.)+?\)/g);
+				var separator;
+				var number_requested;
+				if(parameters)
+				{
+					for(var j = 0; j < parameters.length; j++)
+					{
+					var parameter = parameters[j].toString().replace('(','').replace(')','');
+					if(typeof(parameter)=="number")	
+						number_requested = parameter;
+					else
+						separator = parameter;
+					}
+				}
+				
+				document.write('<br />');
+			}
+			
+			// for (var i = 0; i < templates.length; i++)
+			// {
+							 // document.write(templates[i] + '<br />');
+
+				// var parameters = templates[i].match(/\((.)+?\)/g);
+				// if(parameters)
+				// {
+				// for(var j = 0; j < parameters.length; j++)
+				// {
+									 // document.write(parameters[j] + '<br />');
+
+				// }
+					// }						 document.write('<br />');
+
+			// }
+
+			for (key in values)
+			{
+				for (var i = 0; i < values[key].length; i++)
+				{
+					//$('#resume').html($('#resume').html().replace('[template:' + key+ ']',values[key][i]));
+				}
+			}
+			//$("#profile").html(JSON.stringify(result));
+			//$("#profile").html(templates.length);
+			
+		});
+			});
+			
+			}
+     
+
+  </script>
   </body>
   </html>
--- resamaze.sql
+++ resamaze.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 15, 2012 at 10:09 PM
+-- Generation Time: May 19, 2012 at 03:46 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `templates` (
 --
 
 INSERT INTO `templates` (`id`, `template`) VALUES
-(1, '<div id="resume"><div id="header">[template:phoneNumber]&nbsp;[template:firstName]&nbsp;[template:lastName]&nbsp;[template:email]</div><div class="skills"><strong>SKILLS:</strong><br />[template:skills.skill.name[, ]]</div><div class="positions"><div class="position">[template:positions.position.title[, ]][template:positions.position.company.name[, ]][template:positions.position.company.location[, ]][template:positions.position.startDate.month[, ]][template:positions.position.startDate.year[ - ]][template:positions.position.startDate.month[, ]][template:positions.position.startDate.year[ - ]]<div>[template:positions.position.summary]</div></div></div><div id="footer"><div>[template:mainAddress]</div><div>[template:educations.education.degree][template:educations.education.fieldOfStudy[ from ]][template:educations.education.schoolName][template:educations.education.endDate.year]</div></div></div>');
+(1, '<div id="resume"><div id="header">[template:phoneNumbers.phoneNumber]&nbsp;[template:firstName]&nbsp;[template:lastName]&nbsp;[template:email]</div><div class="skills"><strong>SKILLS:</strong><br />[template:skills.skill.name(, )]</div><div class="positions"><div class="position">[template:positions.title(, )][template:positions.company.name(, )][template:positions.company.location(, )][template:positions.startDate.month(, )][template:positions.startDate.year( - )][template:positions.startDate.month(, )][template:positions.startDate.year( - )]<div>[template:positions.summary]</div></div></div><div id="footer"><div>[template:mainAddress]</div><div>[template:educations.education.degree][template:educations.education.fieldOfStudy( from )][template:educations.education.schoolName][template:educations.education.endDate.year]</div></div></div>');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
