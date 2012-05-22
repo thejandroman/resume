e1d02bfd8d5e94171437f12b6988ed6794b37d9c
Mike Anderson <singularityneuromancer@gmail.com>
2012-05-22 18:59:42 -0400

    moving to desktop

--- resamaze.sql
+++ resamaze.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 19, 2012 at 03:46 PM
+-- Generation Time: May 22, 2012 at 10:58 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -26,7 +26,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `templates`
 --
 
-DROP TABLE IF EXISTS `templates`;
 CREATE TABLE IF NOT EXISTS `templates` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `template` varchar(8000) NOT NULL,
@@ -38,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `templates` (
 --
 
 INSERT INTO `templates` (`id`, `template`) VALUES
-(1, '<div id="resume"><div id="header">[template:phoneNumbers.phoneNumber]&nbsp;[template:firstName]&nbsp;[template:lastName]&nbsp;[template:email]</div><div class="skills"><strong>SKILLS:</strong><br />[template:skills.skill.name(, )]</div><div class="positions"><div class="position">[template:positions.title(, )][template:positions.company.name(, )][template:positions.company.location(, )][template:positions.startDate.month(, )][template:positions.startDate.year( - )][template:positions.startDate.month(, )][template:positions.startDate.year( - )]<div>[template:positions.summary]</div></div></div><div id="footer"><div>[template:mainAddress]</div><div>[template:educations.education.degree][template:educations.education.fieldOfStudy( from )][template:educations.education.schoolName][template:educations.education.endDate.year]</div></div></div>');
+(1, '<div id="resume"><div id="header">[[template:phoneNumbers.phoneNumber]] [[template:firstName]] [[template:lastName]] [[template:email]]</div><div class="skills"><strong>SKILLS:</strong><br />[[template:skills.skill.name(, )]]</div><div class="positions"><div class="position">[[template:positions.title(, )][template:positions.company.name(, )][template:positions.company.location(, )][template:positions.startDate.month(, )][template:positions.startDate.year( - )][template:positions.endDate.month(, )][template:positions.endDate.year( - )][(<div>)template:positions.summary(</div>)]]</div></div><div id="footer"><div>[[template:mainAddress]]</div><div>[[template:educations.education.degree][template:educations.education.fieldOfStudy( from )][template:educations.education.schoolName][template:educations.education.endDate.year]]</div></div></div>');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
